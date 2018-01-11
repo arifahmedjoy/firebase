48165b569339596980eb3a350dbd40afb4083af5
Arif Ahmed Joy <joy_joy@live.com>
2018-01-11 15:16:30 +0600

    Added WP Boilerplate

---
+++ public/js/firebase-public.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDRZgadXQnt-QCi58K74ZdSjcWofM_0dWQ",
+    authDomain: "customer-db-ef864.firebaseapp.com",
+    databaseURL: "https://customer-db-ef864.firebaseio.com",
+    projectId: "customer-db-ef864",
+    storageBucket: "customer-db-ef864.appspot.com",
+    messagingSenderId: "822096415910"
+  };
+  firebase.initializeApp(config);
